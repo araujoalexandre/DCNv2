bcd53b2ccd31a2f583537851fb365e587b123f4f
Alexandre Araujo <aaraujo001@gmail.com>
2021-11-24 18:55:37 +0100

    add class DCN_sep
    The class can be find from https://github.com/Algolzw/EBSR/blob/main/model/DCNv2/dcn_v2.py#L201

--- dcn_v2.py
+++ dcn_v2.py
@@ -173,6 +173,53 @@ class DCN(DCNv2):
             self.deformable_groups,
         )
 
+class DCN_sep(DCNv2):
+    '''Use other features to generate offsets and masks'''
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride,
+                 padding,
+                 dilation=1,
+                 deformable_groups=1):
+        super(DCN_sep, self).__init__(in_channels, out_channels, kernel_size, stride, padding,
+                                      dilation, deformable_groups)
+
+        channels_ = self.deformable_groups * 3 * self.kernel_size[0] * self.kernel_size[1]
+        self.conv_offset_mask = nn.Conv2d(
+            self.in_channels,
+            channels_,
+            kernel_size=self.kernel_size,
+            stride=self.stride,
+            padding=self.padding,
+            bias=True)
+        self.init_offset()
+
+    def init_offset(self):
+        self.conv_offset_mask.weight.data.zero_()
+        self.conv_offset_mask.bias.data.zero_()
+
+    def forward(self, input, fea):
+        '''input: input features for deformable conv
+        fea: other features used for generating offsets and mask'''
+        out = self.conv_offset_mask(fea)
+        o1, o2, mask = torch.chunk(out, 3, dim=1)
+        offset = torch.cat((o1, o2), dim=1)
+        offset = torch.clamp(offset, -200, 200)
+
+        offset_mean = torch.mean(torch.abs(offset))
+        if offset_mean > 150:
+            print('Offset mean is {}, larger than 100.'.format(offset_mean))
+            offset[offset>=150] = 1e-3
+
+        mask = torch.sigmoid(mask)
+        return dcn_v2_conv(input, offset, mask, self.weight, self.bias, self.stride, self.padding,
+                           self.dilation, self.deformable_groups)
+
+
+
 
 class _DCNv2Pooling(Function):
     @staticmethod
